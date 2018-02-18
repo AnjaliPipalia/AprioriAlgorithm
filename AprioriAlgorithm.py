0004f42ed43a6b7ca45b24563dd9ce1d9669e780
AnjaliPipalia <arp226@njit.edu>
2018-02-18 17:29:55 -0500

    Generated association rule after verifying confidence value

--- AprioriAlgorithm.py
+++ AprioriAlgorithm.py
@@ -10,6 +10,7 @@ def generate_frequent_itemsets(candidate_itemsets, support, no_of_transactions):
     for item, frequency in candidate_itemsets.items():
         if more_than_equal_to_min_support(frequency, support, no_of_transactions):
             freq_items.append(item)
+
     return freq_items
 
 
@@ -34,17 +35,57 @@ def join(frequent_itemsets, size):
     return output
 
 
+def get_combinations(itemset):
+    output = {}
+    items = itemset.split(",")
+    for item in items:
+        output[item] = [i for i in items if i != item]
+    for size in range(2, len(items)):
+        keys = list(combinations(items, size))
+        for key in keys:
+            output[key] = [i for i in items if i not in key]
+    return output
+
+
+def get_support(XY, c_itemsets, transactions):
+    xy = XY.split(",")
+    xy.sort()
+    # print(xy)
+    for key, value in c_itemsets.items():
+        c_key = key.split(',')
+        c_key.sort()
+        if c_key == xy:
+            return value
+    print("OMG")
+
+
 class Apriori():
 
     def run(self, support, confidence, transactions, items):
+
         candidate_itemsets = {}
         frequent_itemsets = list()
 
         for i in range(len(items)):
             temp = generate_candidate_itemsets(transactions, items)
-            frequent_itemsets = generate_frequent_itemsets(temp, support, len(transactions))
-            items = join(frequent_itemsets, i+2)
+            temp_frequent_itemsets = generate_frequent_itemsets(temp, support, len(transactions))
+            items = join(temp_frequent_itemsets, i + 2)
             candidate_itemsets = {**candidate_itemsets, **temp}
+            frequent_itemsets += temp_frequent_itemsets
             if not items:
                 break
+        for itemset in frequent_itemsets:
+            combinations = get_combinations(itemset)
+            for X, Y in combinations.items():
+                if len(Y) == 0:
+                    continue
+                strX = X
+                strY = ','.join(Y)
+                if type(X) == tuple:
+                    strX = ','.join(X)
+                strXY = strX + ',' + strY
+                supportXY = get_support(strXY, candidate_itemsets, transactions)
+                supportX = get_support(strX, candidate_itemsets, transactions)
+                if (supportXY / supportX)*100 >= int(confidence):
+                    print(strX + " ---> " + strY)
         return frequent_itemsets
